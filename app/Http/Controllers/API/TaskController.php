9da76acf4376d0fe73f7621a6864e0e8563752f3
SKSritharan <sk.sritharan75@gmail.com>
2025-02-22 11:38:01 +0530

    Added task controller

--- app/Http/Controllers/API/TaskController.php
+++ app/Http/Controllers/API/TaskController.php
@@ -5,17 +5,23 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Task\StoreTaskRequest;
 use App\Http\Requests\Task\UpdateTaskRequest;
+use App\Http\Resources\TaskResource;
 use App\Models\Task;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
 
 class TaskController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index(Request $request)
+    public function index()
     {
-        //
+        return Response::success(
+            TaskResource::collection(
+                Task::latest()->paginate(5)
+            )
+        );
     }
 
     /**
@@ -31,15 +37,25 @@ class TaskController extends Controller
      */
     public function store(StoreTaskRequest $request)
     {
-        //
+        try {
+            $task = Task::create($request->validated());
+            return Response::successMessage(__('responses.task.created'));
+        } catch (\Exception $e) {
+            return Response::errorMessage($e->getMessage());
+        }
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Task $task)
+    public function show($taskId)
     {
-        //
+        try {
+            $task = Task::findOrFail($taskId);
+            return Response::success(new TaskResource($task));
+        } catch (\Exception $e) {
+            return Response::errorMessage(__('responses.task.not_found'));
+        }
     }
 
     /**
@@ -55,14 +71,38 @@ class TaskController extends Controller
      */
     public function update(UpdateTaskRequest $request, Task $task)
     {
-        //
+        try {
+            $task->update($request->validated());
+            return Response::successMessage(__('responses.task.updated'));
+        } catch (\Exception $e) {
+            return Response::errorMessage($e->getMessage());
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Task $task)
+    public function destroy($taskId)
     {
-        //
+        try {
+            $task = Task::findOrFail($taskId);
+            $task->delete();
+            return Response::successMessage(__('responses.task.deleted'));
+        } catch (\Exception $e) {
+            return Response::errorMessage(__('responses.task.not_found'));
+        }
+    }
+
+    /**
+     * mark task as completed
+     */
+    public function markAsCompleted(Task $task)
+    {
+        try {
+            $task->update(['completed' => true]);
+            return Response::successMessage(__('responses.task.completed'));
+        } catch (\Exception $e) {
+            return Response::errorMessage(__('responses.task.not_found'));
+        }
     }
 }
